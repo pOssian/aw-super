2b01043a59c9e389d9611cd4a7bae1a7f6d748d4
possian <p.ossian@icloud.com>
2022-06-30 12:30:51 +0200

    her skriver jeg beskrivelsen

--- Undermappe/Ny_test.py
+++ Undermappe/Ny_test.py
@@ -1,1 +1,8 @@
-git config --global color.ui true
+create table north (
+north_id SERIAL primary key,
+row_1 varchar(30),
+row_2 varchar(30),
+row_3 varchar(30),
+sid INT REFERENCES northwest (sid)
+);
+
